8d39b68fee037f1c95d56941861972aa5c8f12a7
Semisvetiiik <machaeva1988@mail.ru>
2021-02-24 21:32:50 +0900

    created 4/loops/py

---
+++ 4.loops.py
@@ -0,0 +1,25 @@
+# *** циклы (операторы циклов) ***
+
+# while
+
+# перерывание бесконечного цикла ctrl+c
+# while True:
+    # print("Hi")
+
+# бесконечный цикл с перерыванием
+# while True:
+#     _in_data = input("введите что-нибудь: ")
+#     if _in_data == "stop":
+#         print("buy!")
+#         break
+#     print("hi")
+
+ 
+ 
+ # цикл с условием
+ 
+#  number = 0
+
+#  while number < 10:
+#      print(f"Number: {number}") 
+#      number = number + 1
